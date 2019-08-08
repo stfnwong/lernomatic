dca1a24a99b17ceb7415c2499f2ad08526f9fd9c
Stefan Wong <stfnwong@gmail.com>
2019-08-08 13:27:07 +1200

    Changed default checkpoint name format for Pix2PixTrainer

--- lernomatic/train/gan/pix2pix_trainer.py
+++ lernomatic/train/gan/pix2pix_trainer.py
@@ -147,7 +147,7 @@ class Pix2PixTrainer(trainer.Trainer):
             # save checkpoints during training
             if self.save_every > 0 and (self.loss_iter % self.save_every) == 0:
                 ck_name = self.checkpoint_dir + '/' + self.checkpoint_name +\
-                    '_iter_' + str(self.loss_iter) + '_epoch_' + str(self.cur_epoch) + '.pkl'
+                     '_epoch_' + str(self.cur_epoch) + '_iter_' + str(self.loss_iter) + '.pkl'
                 if self.verbose:
                     print('\t Saving checkpoint to file [%s] ' % str(ck_name))
                 self.save_checkpoint(ck_name)
