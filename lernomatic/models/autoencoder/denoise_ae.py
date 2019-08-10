8922503645d1e3ad385895ce627dff60ad2a6b00
Stefan Wong <stfnwong@gmail.com>
2019-08-10 23:18:58 +1200

    Fixed shape issues in DAE modules

--- lernomatic/models/autoencoder/denoise_ae.py
+++ lernomatic/models/autoencoder/denoise_ae.py
@@ -11,16 +11,20 @@ import torch
 import torch.nn as nn
 from lernomatic.models import common
 
+from pudb import set_trace; set_trace()
+
 
 # ======== ENCODER SIDE ======== #
 class DAEEncoderBlock(nn.Module):
     def __init__(self, in_channels:int, out_channels:int,
                  kernel_size:int=3, stride:int=1, padding:int=1) -> None:
         super(DAEEncoderBlock, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
 
         self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
+            self.in_channels,
+            self.out_channels,
             kernel_size = kernel_size,
             stride = stride,
             padding = padding
@@ -52,13 +56,19 @@ class DAEEncoderModule(nn.Module):
         # "Input" half of encoder
         in_blocks = []
         cur_channel_size = self.start_size
+        print('DAEEncoder')
         for n in range(self.num_blocks):
-            print(n, cur_channel_size)
             if n == 0:
                 block = DAEEncoderBlock(
                     1,
                     cur_channel_size,
                     kernel_size = self.kernel_size,
+                )
+            elif (n > 0) and (n % 2 != 0):
+                block = DAEEncoderBlock(
+                    cur_channel_size,
+                    cur_channel_size,
+                    kernel_size = self.kernel_size
                 )
             else:
                 block = DAEEncoderBlock(
@@ -66,23 +76,33 @@ class DAEEncoderModule(nn.Module):
                     int(2 * cur_channel_size),
                     kernel_size = self.kernel_size
                 )
-            in_blocks += [block]
-            cur_channel_size = 2 * cur_channel_size
+                cur_channel_size = 2 * cur_channel_size
 
+            in_blocks += [block]
+            print(n, block.in_channels, block.out_channels)
 
         # "output" half. This is one block shorter than input size
         out_blocks = []
         for n in range(self.num_blocks - 1):
-            block = DAEEncoderBlock(
-                cur_channel_size,
-                int(2 * cur_channel_size),
-                kernel_size = self.kernel_size
-            )
+            if (n > 0) and (n % 2 != 0):
+                block = DAEEncoderBlock(
+                    cur_channel_size,
+                    cur_channel_size,
+                    kernel_size = self.kernel_size
+                )
+            else:
+                block = DAEEncoderBlock(
+                    cur_channel_size,
+                    int(2 * cur_channel_size),
+                    kernel_size = self.kernel_size
+                )
+                cur_channel_size = 2 * cur_channel_size
+
             out_blocks += [block]
             if n == (self.num_blocks - 2):
                 out_blocks += [nn.MaxPool2d(2, 2)]
 
-            cur_channel_size = 2 * cur_channel_size
+            print(n, block.in_channels, block.out_channels)
 
         self.in_blocks = nn.Sequential(*in_blocks)
         self.pool = nn.MaxPool2d(2, 2)
@@ -91,7 +111,8 @@ class DAEEncoderModule(nn.Module):
     def forward(self, X:torch.Tensor) -> torch.Tensor:
         out = self.in_blocks(X)
         out = self.pool(out)
-        out = self.out_blocks(X)
+        out = self.out_blocks(out)
+        #out = out.view(X.shape[0], -1)
 
         return out
 
@@ -140,15 +161,19 @@ class DAEEncoder(common.LernomaticModel):
 # ======== DECODER SIDE ======== #
 class DAEDecoderBlock(nn.Module):
     def __init__(self, in_channels:int, out_channels:int,
-                 kernel_size:int=3, stride:int=1, padding:int=1) -> None:
+                 kernel_size:int=3, stride:int=1, padding:int=1,
+                 out_padding:int=0) -> None:
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         super(DAEDecoderBlock, self).__init__()
 
         self.conv = nn.ConvTranspose2d(
-            in_channels,
-            out_channels,
+            self.in_channels,
+            self.out_channels,
             kernel_size= kernel_size,
             stride = stride,
-            padding = padding
+            padding = padding,
+            output_padding = out_padding
         )
         self.relu = nn.ReLU()
         self.bn   = nn.BatchNorm2d(out_channels)
@@ -173,39 +198,65 @@ class DAEDecoderModule(nn.Module):
 
         in_blocks = []
         cur_channel_size = self.start_size
+        print('DAEDecoder')
         for n in range(self.num_blocks):
-            block = DAEDecoderBlock(
-                cur_channel_size,
-                int(cur_channel_size // 2),
-                kernel_size = self.kernel_size
-            )
+            if (n > 0) and (n % 2 != 0):
+                block = DAEDecoderBlock(
+                    cur_channel_size,
+                    cur_channel_size,
+                    kernel_size = self.kernel_size,
+                )
+            else:
+                block = DAEDecoderBlock(
+                    cur_channel_size,
+                    int(cur_channel_size // 2),
+                    kernel_size = self.kernel_size,
+                    stride = 2 if (n == 0) else 1,
+                    out_padding = 1 if (n == 0) else 0
+                )
+                cur_channel_size = cur_channel_size // 2
+
             in_blocks += [block]
-            cur_channel_size = cur_channel_size // 2
+            print(n, block.in_channels, block.out_channels)
 
         out_blocks = []
         for n in range(self.num_blocks-2):
-            block = DAEDecoderBlock(
-                cur_channel_size,
-                int(cur_channel_size // 2),
-                kernel_size = self.kernel_size
-            )
+            if (n > 0) and (n % 2 != 0):
+                block = DAEDecoderBlock(
+                    cur_channel_size,
+                    cur_channel_size,
+                    kernel_size = self.kernel_size
+                )
+            else:
+                block = DAEDecoderBlock(
+                    cur_channel_size,
+                    int(cur_channel_size // 2),
+                    kernel_size = self.kernel_size
+                )
+                cur_channel_size = cur_channel_size // 2
+            out_blocks += [block]
+            print(n, block.in_channels, block.out_channels)
+
 
         final_block = [
             nn.ConvTranspose2d(
                 cur_channel_size,
                 1,
                 kernel_size = self.kernel_size,
+                stride = 2,
                 padding = 1,
-                stride = 1
+                output_padding = 1
             ),
             nn.ReLU()
         ]
+        print(n+1, cur_channel_size, 1)
 
         self.in_blocks   = nn.Sequential(*in_blocks)
         self.out_blocks  = nn.Sequential(*out_blocks)
         self.final_block = nn.Sequential(*final_block)
 
     def forward(self, X:torch.Tensor) -> torch.Tensor:
+        #out = X.view(X.shape[0], self.start_size, -1)
         out = self.in_blocks(X)
         out = self.out_blocks(out)
         out = self.final_block(out)
