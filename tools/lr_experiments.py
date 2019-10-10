4674db2b8f6e1cac289760f2d4ce3878e00971de
Stefan Wong <stfnwong@gmail.com>
2019-10-10 18:02:34 +1300

    Seems to work... some of the KDE plots are a bit suspect. Need more lr_finder history to test.

--- tools/lr_experiments.py
+++ tools/lr_experiments.py
@@ -50,23 +50,29 @@ def sum_segments(X:np.ndarray, num_segments:int=8) -> np.ndarray:
 
 # ======== KDE ======== #
 
-# Normal density
+# Normal density function
 def phi(X:np.ndarray) -> np.ndarray:
-    return np.exp(-0.5 * np.square(X)) / np.sqrt(2 * np.pi)
+    return np.square(np.exp(-0.5 * X) / np.sqrt(2 * np.pi))
 
 # h is bandwidth parameter
+def kernel_elem(X:float, data:float, h:float) -> np.ndarray:
+    return np.mean(phi((X - data) / h) / h)
+
 def kernel(domain:np.ndarray, data:np.ndarray, h:float) -> np.ndarray:
-    return phi((domain - data) / h) / h
-    #return np.mean(phi((domain - data) / h) / h)
+    return np.fromiter((kernel_elem(domain, xi, h) for xi in data), data.dtype)
+    #return np.array(list(map(kernel_elem, domain, data, h)))
 
-#def kernel_elem(X:np.ndarray, data:np.ndarray, h:float) -> np.ndarray:
-#    pass
 
+# NOTE: this seems to just be producing the inverse of the acc_history data.
+# That is, the 'density' is just X mirrored about the y-axis
 def kernel_density_est(data:np.ndarray) -> np.ndarray:
-
     # silverman bandwidth
-    h = np.std(data) * np.power(((4 / 3) * np.prod(data.shape)), 1/5)
-    #domain = np.arange(len(data))
+    # NOTE: I think I actually want a much larger bandwidth here
+    h = np.std(data) * np.power((4 / (3 * np.prod(data.shape))), 1/5)
+    #h = np.power(1.06 * np.std(data) * np.prod(data.shape), 1/5)
+    #h = 0.25
+
+    # evaluate the kernel function over this range
     domain = np.linspace(0, len(data), len(data))
 
     return kernel(domain, data, h)
@@ -90,7 +96,8 @@ def plot_centroid_segments(ax, X:np.ndarray, centroids:np.ndarray, seg_len:int) 
 
 def main() -> None:
     # get a finder from disk
-    finder_state_file = '[resnet][max_acc]_lr_finder_state.pth'
+    #finder_state_file = '[resnet][max_acc]_lr_finder_state.pth'
+    finder_state_file = '[resnet][min_loss]_lr_finder_state.pth'
     lr_finder = lr_common.lr_finder_auto_load(finder_state_file)
 
     print('Loaded lr_finder from file [%s]' % str(finder_state_file))
@@ -99,27 +106,34 @@ def main() -> None:
     # lets clone the accurracy history
     #acc_history = np.asarray(lr_finder.acc_history[400 : 800])
     acc_history = np.asarray(lr_finder.acc_history)
+    kde = kernel_density_est(acc_history)
+
+    # What is the kde of the kde?
+    kde2 = kernel_density_est(kde)
 
     # what is the sum of the bottom half vs the top half?
-    lower_sum = np.sum(acc_history[ : len(acc_history) // 2])
-    upper_sum = np.sum(acc_history[len(acc_history) // 2 : ])
+    #lower_sum = np.sum(acc_history[ : len(acc_history) // 2])
+    #upper_sum = np.sum(acc_history[len(acc_history) // 2 : ])
 
-    print('Sum of acc_history[0 : %d]  : %f' % (len(acc_history) // 2, lower_sum))
-    print('Sum of acc_history[%d : %d] : %f' % (len(acc_history) // 2, len(acc_history), upper_sum))
+    #print('Sum of acc_history[0 : %d]  : %f' % (len(acc_history) // 2, lower_sum))
+    #print('Sum of acc_history[%d : %d] : %f' % (len(acc_history) // 2, len(acc_history), upper_sum))
 
-    num_centroid_seg = 8
-    centroids, seg_len = centroid_segments(acc_history, num_segments=num_centroid_seg)
+    #num_centroid_seg = 8
+    #centroids, seg_len = centroid_segments(acc_history, num_segments=num_centroid_seg)
 
     # make a plot
     fig, ax = plt.subplots()
-    plot_centroid_segments(ax, acc_history, centroids, seg_len)
+    #plot_centroid_segments(ax, acc_history, centroids, seg_len)
 
-
+    ax.plot(kde)
+    #ax.plot(kde2)
+    ax.axvline(x = np.argmax(kde), color='r')
     #ax.plot(acc_history)
     #xm = single_centroid(acc_history)
     #ax.axvline(x = xm, color='r')
 
-    ax.set_title('Accuracy vs. iterations')
+    #ax.set_title('Accuracy vs. iterations')
+    ax.set_title('Acc. history KDE')
     ax.set_xlabel('Iteration')
     ax.set_ylabel('Accuracy')
     fig.tight_layout()
