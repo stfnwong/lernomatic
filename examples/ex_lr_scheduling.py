b38ac3650fd6b1440c1181a728dfcb3a8ae345b8
Stefan Wong <stfnwong@gmail.com>
2019-10-13 00:43:15 +1300

    Added command line options from lernomatic.options

--- examples/ex_lr_scheduling.py
+++ examples/ex_lr_scheduling.py
@@ -19,6 +19,8 @@ from lernomatic.train import cifar_trainer
 from lernomatic.train import schedule
 # vis tools
 from lernomatic.vis import vis_loss_history
+# command line options
+from lernomatic.options import options
 
 # timing stuff
 import time
@@ -55,9 +57,9 @@ def get_lr_finder(trainer, find_type='LogFinder') -> lr_common.LRFinder:
     lr_find_obj = getattr(lr_common, find_type)
     lr_finder = lr_find_obj(
         trainer,
-        lr_min           = GLOBAL_OPTS['lr_min'],
-        lr_max           = GLOBAL_OPTS['lr_max'],
-        lr_select_method = GLOBAL_OPTS['lr_select_method'],
+        lr_min           = GLOBAL_OPTS['find_lr_min'],
+        lr_max           = GLOBAL_OPTS['find_lr_max'],
+        lr_select_method = GLOBAL_OPTS['find_lr_select_method'],
         num_epochs       = GLOBAL_OPTS['find_num_epochs'],
         explode_thresh   = GLOBAL_OPTS['find_explode_thresh'],
         print_every      = GLOBAL_OPTS['find_print_every']
@@ -191,28 +193,28 @@ def main() -> None:
     ]
 
     checkpoint_names = [
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_triangular_sched_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_triangular2_sched_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_exp_decay_sched_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_warm_restart_sched_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_triangular_exp_sched_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_triangular2_exp_sched_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_decay_when_acc_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_triangular_decay_when_acc_cifar10',
-        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_no_sched_cifar10'
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_triangular_sched_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_triangular2_sched_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_exp_decay_sched_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_warm_restart_sched_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_triangular_exp_sched_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_triangular2_exp_sched_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_decay_when_acc_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_triangular_decay_when_acc_cifar10',
+        str(GLOBAL_OPTS['model']) + '_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_no_sched_cifar10'
     ]
 
     figure_dir = 'figures/'
     figure_names = [
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_triangular_sched_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_triangular2_sched_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_exp_decay_sched_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_warm_restart_sched_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_triangular_exp_sched_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_triangular2_exp_sched_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_decay_when_acc_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_triangular_decay_when_acc_cifar10.png',
-        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['lr_select_method']) + ']_ex_no_sched_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_triangular_sched_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_triangular2_sched_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_exp_decay_sched_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_warm_restart_sched_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_triangular_exp_sched_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_triangular2_exp_sched_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_decay_when_acc_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_triangular_decay_when_acc_cifar10.png',
+        figure_dir + '[' + str(GLOBAL_OPTS['model']) + ']_[' + str(GLOBAL_OPTS['find_lr_select_method']) + ']_ex_no_sched_cifar10.png',
     ]
 
     assert len(schedulers) == len(checkpoint_names)
@@ -234,9 +236,9 @@ def main() -> None:
         if idx == 0:
             lr_find_min, lr_find_max, lr_finder = find_lr(trainer, return_finder=True)
             # create plots
-            lr_acc_title  = '[' + str(GLOBAL_OPTS['model']) + '][' + str(GLOBAL_OPTS['lr_select_method']) + '] ' +\
+            lr_acc_title  = '[' + str(GLOBAL_OPTS['model']) + '][' + str(GLOBAL_OPTS['find_lr_select_method']) + '] ' +\
                 str(schedulers[idx]) + ' learning rate vs acc (log)'
-            lr_loss_title = '[' + str(GLOBAL_OPTS['model']) + '][' + str(GLOBAL_OPTS['lr_select_method']) + '] ' +\
+            lr_loss_title = '[' + str(GLOBAL_OPTS['model']) + '][' + str(GLOBAL_OPTS['find_lr_select_method']) + '] ' +\
                 str(schedulers[idx]) + ' learning rate vs loss (log)'
             lr_fig, lr_ax = vis_loss_history.get_figure_subplots(2)
             lr_finder.plot_lr_vs_acc(lr_ax[0], lr_acc_title, log=True)
@@ -244,11 +246,11 @@ def main() -> None:
             # save
             lr_fig.tight_layout()
             lr_fig.savefig('figures/[%s][%s]_%s_lr_finder_output.png' %\
-                           (str(GLOBAL_OPTS['model']), str(GLOBAL_OPTS['lr_select_method']), str(schedulers[idx]))
+                           (str(GLOBAL_OPTS['model']), str(GLOBAL_OPTS['find_lr_select_method']), str(schedulers[idx]))
             )
             # also save state information from finder
             lr_finder_state_file = '[' + str(GLOBAL_OPTS['model']) + '][' +\
-                str(GLOBAL_OPTS['lr_select_method']) + ']_lr_finder_state.pth'
+                str(GLOBAL_OPTS['find_lr_select_method']) + ']_lr_finder_state.pth'
             lr_finder.save(lr_finder_state_file)
 
         print('Found learning rates as %.4f -> %.4f' % (lr_find_min, lr_find_max))
@@ -266,8 +268,8 @@ def main() -> None:
         # create plots
         trainers.append(trainer)
         acc_list.append(trainer.get_acc_history())
-        loss_title = str(schedulers[idx]) + ' ' + str(GLOBAL_OPTS['lr_select_method']) + ' Loss : LR range (%.3f -> %.3f)' % (lr_find_min, lr_find_max)
-        acc_title  = str(schedulers[idx]) + ' ' + str(GLOBAL_OPTS['lr_select_method']) + ' Accuracy : LR range (%.3f -> %.3f)' % (lr_find_min, lr_find_max)
+        loss_title = str(schedulers[idx]) + ' ' + str(GLOBAL_OPTS['find_lr_select_method']) + ' Loss : LR range (%.3f -> %.3f)' % (lr_find_min, lr_find_max)
+        acc_title  = str(schedulers[idx]) + ' ' + str(GLOBAL_OPTS['find_lr_select_method']) + ' Accuracy : LR range (%.3f -> %.3f)' % (lr_find_min, lr_find_max)
         generate_plot(trainer, loss_title, acc_title, figure_names[idx])
         schedule_end_time = time.time()
         schedule_total_time = schedule_end_time - schedule_start_time
@@ -283,12 +285,12 @@ def main() -> None:
         acc_ax.set_ylabel('Accuracy')
         acc_ax.legend(checkpoint_names)
         acc_ax.set_title('[%s] [%s] Accuracy comparison for learning rate schedules (LR: %.4f -> %.4f)' %\
-                        (str(GLOBAL_OPTS['model']), str(GLOBAL_OPTS['lr_select_method']), lr_find_min, lr_find_max)
+                        (str(GLOBAL_OPTS['model']), str(GLOBAL_OPTS['find_lr_select_method']), lr_find_min, lr_find_max)
         )
         acc_fig.tight_layout()
         acc_fig.set_size_inches(10, 10)
         acc_fig.savefig('figures/[%s]_[%s]_ex_lr_scheduling_acc_compare_%.4f_%.4f.png' %\
-                        (str(GLOBAL_OPTS['model']), str(GLOBAL_OPTS['lr_select_method']), lr_find_min, lr_find_max)
+                        (str(GLOBAL_OPTS['model']), str(GLOBAL_OPTS['find_lr_select_method']), lr_find_min, lr_find_max)
         )
 
     expr_end_time = time.time()
@@ -297,7 +299,8 @@ def main() -> None:
 
 
 def get_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser()
+    parser = options.get_trainer_options()
+    parser = options.get_lr_finder_options(parser)
     # General opts
     parser.add_argument('-v', '--verbose',
                         action='store_true',
@@ -308,26 +311,6 @@ def get_parser() -> argparse.ArgumentParser:
                         default=False,
                         action='store_true',
                         help='Display plots'
-                        )
-    parser.add_argument('--print-every',
-                        type=int,
-                        default=100,
-                        help='Print output every N epochs'
-                        )
-    parser.add_argument('--save-every',
-                        type=int,
-                        default=-1,
-                        help='Save model checkpoint every N epochs'
-                        )
-    parser.add_argument('--num-workers',
-                        type=int,
-                        default=1,
-                        help='Number of workers to use when generating HDF5 files'
-                        )
-    parser.add_argument('--find-only',
-                        action='store_true',
-                        default=False,
-                        help='Only perform the parameter find step (no training)'
                         )
     # model, size options
     parser.add_argument('--model',
@@ -340,85 +323,11 @@ def get_parser() -> argparse.ArgumentParser:
                         default=58,
                         help='Depth of resnet to use for resnet models'
                         )
-    # Learning rate finder options
-    parser.add_argument('--find-print-every',
-                        type=int,
-                        default=20,
-                        help='How often to print output from learning rate finder'
-                        )
-    parser.add_argument('--find-num-epochs',
-                        type=int,
-                        default=8,
-                        help='Maximum number of epochs to attempt to find learning rate'
-                        )
-    parser.add_argument('--find-explode-thresh',
-                        type=float,
-                        default=4.5,
-                        help='Threshold at which to stop increasing learning rate'
-                        )
-    parser.add_argument('--lr-min',
-                        type=float,
-                        default=2e-4,
-                        help='Minimum range to search for learning rate'
-                        )
-    parser.add_argument('--lr-max',
-                        type=float,
-                        default=1e-1,
-                        help='Maximum range to search for learning rate'
-                        )
-    parser.add_argument('--lr-select-method',
-                        type=str,
-                        default='kde',
-                        help='Method to use for selecting LR range'
-                        )
     # Schedule options
     parser.add_argument('--exp-decay',
                         type=float,
                         default=0.001,
                         help='Exponential decay term'
-                        )
-    parser.add_argument('--sched-stepsize',
-                        type=int,
-                        default=4000,
-                        help='Size of step for learning rate scheduler'
-                        )
-    # Device options
-    parser.add_argument('--device-id',
-                        type=int,
-                        default=-1,
-                        help='Set device id (-1 for CPU)'
-                        )
-    # Network options
-    # Training options
-    parser.add_argument('--batch-size',
-                        type=int,
-                        default=64,
-                        help='Batch size to use during training'
-                        )
-    parser.add_argument('--val-batch-size',
-                        type=int,
-                        default=64,
-                        help='Batch size to use during testing'
-                        )
-    parser.add_argument('--start-epoch',
-                        type=int,
-                        default=0,
-                        )
-    parser.add_argument('--num-epochs',
-                        type=int,
-                        default=20,
-                        help='Epoch to stop training at'
-                        )
-
-    parser.add_argument('--weight-decay',
-                        type=float,
-                        default=0.0,
-                        help='Weight decay to use for optimizer'
-                        )
-    parser.add_argument('--learning-rate',
-                        type=float,
-                        default=1e-3,
-                        help='Learning rate for optimizer'
                         )
     # Data options
     parser.add_argument('--checkpoint-dir',
